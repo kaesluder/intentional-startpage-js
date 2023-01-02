a73749e436310725548db002614739f71dcaec82
Kae Sluder <kaesluder@fastmail.com>
2023-01-01 23:57:57 -0500

    playing with error checking

--- src/components/WorldTimeConfig.js
+++ src/components/WorldTimeConfig.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { DateTime } from 'luxon';
-import { update } from 'ramda';
+import { update, all, reject } from 'ramda';
 import './WorldTimeConfig.css';
+
+const isTimeZoneValid = function (tzString) {
+  return DateTime.local().setZone(tzString).isValid;
+};
 
 const WorldTimeConfig = function (props) {
   const [configFormState, setConfigFormState] = useState(props.ClockList);
 
   // wire up onChange
   const handleFormChange = function (event) {
-    console.log(event.target.name, event.target.value);
+    // console.log(event.target.name, event.target.value);
     const fieldIndex = parseInt(event.target.name.match(/\d+/)[0]);
     setConfigFormState((configFormState) =>
       update(fieldIndex, event.target.value, configFormState)
@@ -17,7 +21,14 @@ const WorldTimeConfig = function (props) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    props.handleClockEdit(props.id, configFormState);
+    // validate time zones before changing anything
+    if (all(isTimeZoneValid, configFormState)) {
+      props.handleClockEdit(props.id, configFormState);
+    } else {
+      // display a list of time zones that are not valid.
+      const invalids = reject(isTimeZoneValid, configFormState);
+      alert(`invalid time zones: ${invalids.join(', ')}`);
+    }
   };
 
   const createTextField = function (timeZone, i) {
