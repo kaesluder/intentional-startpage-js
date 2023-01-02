3369623875ab70196f7de6c7a7d65cfc3f47e508
Kae Sluder <kaesluder@fastmail.com>
2023-01-02 15:37:14 -0500

    changing time zone working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { find, assoc, propEq, findIndex, update, partial } from 'ramda';
 import 'bulma/css/bulma.min.css';
 import PageHeading from './components/PageHeading';
 import WorldTime from './components/WorldTime';
@@ -43,6 +44,16 @@ const pageHeadingText = localStorage.getItem('pageHeadingText')
   ? localStorage.getItem('pageHeadingText')
   : 'Hello World!';
 
+const findWidgetByid = (id, xs) => find(propEq('id', id), xs);
+
+const replaceWidget = function (newWidget, widgetList) {
+  const index = findIndex(propEq('id', newWidget.id), widgetList);
+  // console.log(`replaceWidget ${newWidget.id}`);
+
+  // console.log(`replaceWidget ${index}`);
+  return update(index, newWidget, widgetList);
+};
+
 function App() {
   const [pageHeadingState, setPageHeading] = useState(pageHeadingText);
   const pageHeadingEditHandler = function (newText) {
@@ -55,7 +66,16 @@ function App() {
 
   const handleClockEdit = function (id, clockList) {
     console.log(`handleClockEdit: ${id}`);
-    console.log(clockList);
+    const newWidgetSpec = assoc(
+      'clockList',
+      clockList,
+      findWidgetByid(id, configState)
+    );
+
+    // console.log(JSON.stringify(replaceWidget(newWidgetSpec, configState)));
+
+    //    setConfigState(replaceWidget(newWidgetSpec, configState));
+    setConfigState(partial(replaceWidget, [newWidgetSpec]));
   };
 
   return (
